c2cf04410655a6be974acc1cf68843055ea87831
Roy <0247152@up.edu.mx>
2025-11-21 19:31:44 -0600

    host render agregado

--- xocolatl_system/settings.py
+++ xocolatl_system/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY = 'django-insecure-4)m1%tp8qq27g!^$$&$2=afmve@07-%jecn5=&_7t#lr^z--gb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
